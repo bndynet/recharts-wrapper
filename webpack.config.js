c92ed5508604862caeccaf1f77331d1058c938bc
Bendy Zhang <zb@bndy.net>
2019-01-13 21:19:43 +0800

    debug for es6

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,4 @@
 const path = require("path");
-const webpack = require("webpack");
-const app = require("./package.json");
-// const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const CleanWebpackPlugin = require("clean-webpack-plugin");
-// const CopyWebpackPlugin = require("copy-webpack-plugin");
-// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-// const PrintTimeWebpackPlugin = require("print-time-webpack");
 const HeaderInjectionWebpackPlugin = require("@bndynet/header-injection-webpack-plugin");
 
 module.exports = {
